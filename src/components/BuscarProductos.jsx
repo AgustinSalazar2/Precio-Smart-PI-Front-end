18da913a74da1ca743ca0bf0e0ebafcc0207db61
marnvel <ricardom.velarde@gmail.com>
2022-11-29 11:06:19 -0300

    Se añadieron selects del filtro para la UI de cliente. 29-11-2022 11:06hs

--- src/components/BuscarProductos.jsx
+++ src/components/BuscarProductos.jsx
@@ -46,48 +46,93 @@ export const BuscarProductos = () => {
   
   //Renderizado
   return (
+
     <>
-      <div className='container justify-content-center col-lg-8 mt-5'>
-        <div className='container col-lg-6 justify-content-center'>
-          <input
-          value={search}
-          onChange={buscador}
-          type="text"
-          placeholder='search' 
-          className='form-control mt-3' />
+      <div className="container justify-content-center col-lg-8 mt-5">
+
+        <div className="container">
+          <div className="row">
+
+            <div className="col-lg-3 justify-content-center ">
+              <div className="mb-3">
+                {/* <label htmlFor="" class="form-label">
+                  Zona
+                </label> */}
+                <select className="form-select" name="" id="">
+
+                  <option defaultValue value='zona_1'>Zona 1</option>
+                  <option value='zona_2'>Zona 2</option>
+                  <option value='zona_3'>Zona 3</option>
+                  <option value='zona_4'>Zona 4</option>
+                  <option value='zona_5'>Zona 5</option>
+                  <option value='zona_6'>Zona 6</option>
+                  <option value='zona_7'>Zona 7</option>
+                  <option value='zona_8'>Zona 8</option>
+                  <option value='zona_9'>Zona 9</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="container col-lg-3 justify-content-center ">
+              <div className="mb-3">
+                {/* <label htmlFor="" class="form-label">
+                  Zona
+                </label> */}
+                <select className="form-select form-select" name="" id="">
+
+                  {/* <option defaultValue>Categoría</option> */}
+                  <option defaultValue value="">Comestibles</option>
+                  <option value="bebidas">Bebidas</option>
+                  <option value="limpieza">Limpieza</option>
+                  <option value="otros">Otros</option>
+
+                </select>
+              </div>
+            </div>
+
+            <div className="col-lg-6">
+              <input
+                value={search}
+                onChange={buscador}
+                type="text"
+                placeholder="search"
+                className="form-control mb-3"
+                autoFocus="true"
+              />
+
+            </div>
+
+
+          </div>
         </div>
-        <table className='table table-striped table-hover mt-5 shadow-lg table-control'>
+
+        <table className="table table-striped table-hover mt-5 shadow-lg table-control">
           <thead>
-            {
-              (search)
-              ? 
-                <tr>
-                  <th>Nombre</th>
-                  <th>Marca</th>
-                  <th>Presentación</th>
-                  <th>Precio</th>
-                </tr>
-              
-              : <tr></tr>
-            }
+            {search ? (
+              <tr>
+                <th>Nombre</th>
+                <th>Marca</th>
+                <th>Presentación</th>
+                <th>Precio</th>
+              </tr>
+            ) : (
+              <tr></tr>
+            )}
           </thead>
 
           <tbody>
-            
-            {
-              results.map((prod)=>(
-                <tr key={prod._id}>
-                  <td>{prod.productName}</td>
-                  <td>{prod.marca}</td>
-                  <td>{prod.presentacion}</td>
-                  <td>${prod.precio}</td>
-                </tr>
-              ))
-            }
-
+            {results.map((prod) => (
+              <tr key={prod._id}>
+                <td>{prod.productName}</td>
+                <td>{prod.marca}</td>
+                <td>{prod.presentacion}</td>
+                <td>${prod.precio}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </>
+    
   )
 }
